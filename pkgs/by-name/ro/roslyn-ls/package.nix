07d1453926b06c5a9097d2645fe1533100f049f4
Konrad Malik <konrad.malik@gmail.com>
2024-02-03 11:58:37 +0100

    roslyn-ls: 4.9.0-3 -> 4.10.0-1

--- pkgs/by-name/ro/roslyn-ls/package.nix
+++ pkgs/by-name/ro/roslyn-ls/package.nix
@@ -1,51 +1,59 @@
-{ lib, fetchFromGitHub, buildDotnetModule, dotnetCorePackages, testers, roslyn-ls }:
+{ lib, fetchFromGitHub, buildDotnetModule, dotnetCorePackages, stdenvNoCC, testers, roslyn-ls }:
 let
   pname = "roslyn-ls";
-  vsVersion = "2.12.19";
+  vsVersion = "2.16.24";
   dotnet-sdk = dotnetCorePackages.sdk_8_0;
-  # need sdk on runtime otherwise most function won't work
+  # need sdk on runtime as well
   dotnet-runtime = dotnet-sdk;
+
   project = "Microsoft.CodeAnalysis.LanguageServer";
-  srcPath = "src/Features/LanguageServer/${project}";
+
 in
 buildDotnetModule {
   inherit pname dotnet-sdk dotnet-runtime;
-
-  # versioned independently from vscode-csharp
-  # read from https://github.com/dotnet/roslyn/blob/main/eng/Versions.props
-  version = "4.9.0-3";
 
   src = fetchFromGitHub {
     owner = "dotnet";
     repo = "roslyn";
     rev = "VSCode-CSharp-${vsVersion}";
-    hash = "sha256-S3bRIgtQ7VIA3PYxnSlo3DxwmwExWsRi1STU+KGrFPM=";
+    hash = "sha256-pTZ8cEqDupfZn6SfVd38mJnDEABvXW1S3xaRd0LryoY=";
   };
 
-  projectFile = "${srcPath}/${project}.csproj";
-  nugetDeps = ./deps.nix;
-  dotnetBuildFlags = [ "--output ./out" ];
+  # versioned independently from vscode-csharp
+  # read from https://github.com/dotnet/roslyn/blob/main/eng/Versions.props
+  version = "4.10.0-1";
+  projectFile = "src/Features/LanguageServer/${project}/${project}.csproj";
   useDotnetFromEnv = true;
+  nugetDeps = ./deps.nix;
 
-  # not needed after update
   postPatch = ''
-    sed -i 's/"rollForward": "disable"/"rollForward": "patch"/' global.json
+    substituteInPlace $projectFile \
+      --replace-fail \
+        '<RuntimeIdentifiers>win-x64;win-x86;win-arm64;linux-x64;linux-arm64;alpine-x64;alpine-arm64;osx-x64;osx-arm64</RuntimeIdentifiers>' \
+        '<RuntimeIdentifiers>linux-x64;linux-arm64;osx-x64;osx-arm64</RuntimeIdentifiers>'
   '';
 
-  # override because "error NETSDK1085: The 'NoBuild' property was set to true but the 'Build' target was invoked."
-  installPhase = ''
-    runHook preInstall
+  # custom installPhase with --no-build removed
+  # BuildHost project within roslyn is running Build target during publish
+  installPhase =
+    let
+      rid = dotnetCorePackages.systemToDotnetRid stdenvNoCC.targetPlatform.system;
+    in
+    ''
+      runHook preInstall
 
-    mkdir -p $out/lib
-    mv ./out $out/lib/${pname}
+      dotnet publish $projectFile \
+          -p:ContinuousIntegrationBuild=true \
+          -p:Deterministic=true \
+          -p:InformationalVersion=$version \
+          -p:UseAppHost=true \
+          -p:PublishTrimmed=false \
+          --no-self-contained \
+          --output "$out/lib/$pname" \
+          --runtime ${rid}
 
-    runHook postInstall
-  '';
-
-  postFixup = ''
-    mv $out/bin/${project} $out/bin/${pname}
-    rm $out/bin/Microsoft*
-  '';
+      runHook postInstall
+    '';
 
   passthru = {
     tests.version = testers.testVersion { package = roslyn-ls; };
@@ -57,6 +65,6 @@ buildDotnetModule {
     changelog = "https://github.com/dotnet/vscode-csharp/releases/tag/v${vsVersion}";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ konradmalik ];
-    mainProgram = "roslyn-ls";
+    mainProgram = project;
   };
 }
